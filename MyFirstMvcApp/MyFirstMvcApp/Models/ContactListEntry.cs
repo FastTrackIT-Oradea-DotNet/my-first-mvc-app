fc8ceedf7a775684f96abb7cd5ea742907ba2799
Florin Lazar <florin.lazar.it2014@gmail.com>
2020-08-12 21:11:50 +0300

    my first mvc app

---
+++ MyFirstMvcApp/MyFirstMvcApp/Models/ContactListEntry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstMvcApp.Models
+{
+    public class ContactListEntry
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Display(Name = "Contact Type")]
+        [Required]
+        public ContactType ContactType { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [StringLength(50)]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Email")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
